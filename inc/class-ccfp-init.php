b96505cf124895d423b96b6c6cc169213294ea38
Alvind <alvind.caesar@gmail.com>
2022-01-25 13:48:25 +0800

    Initial Commit

---
+++ inc/class-ccfp-init.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Main Init Class
+ *
+ * @package     CCFP
+ * @subpackage  CCFP/includes
+ * @copyright   Copyright (c) 2022, Alvind Caesar
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.0.0
+ * @author      Alvind CAesar <hello@alvindcaesar.com>
+ */
+
+class CCFP_Init {
+
+	/**
+	 * Initialize the class
+	 */
+	public function __construct() {
+
+		// $register_post_types     = new CFT_Register_Post_Types();
+		
+		
+	}
+
+}
